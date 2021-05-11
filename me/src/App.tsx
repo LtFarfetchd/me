2d1a6a467f140fee010718812a1645abaf79a44f
Adam Judd <adam.judd@rea-group.com.au>
2021-05-11 13:44:56 +1000

    Update usage of props and switch site to black

--- me/src/App.tsx
+++ me/src/App.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import { colors } from "./Helpers/palette";
 import { GradientButton } from "./components/Button";
+import { sizes } from "./Helpers/sizes";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <GradientButton
-          colorOne={colors.primaryColor}
-          colorTwo={colors.secondaryColor}
-          borderSize="0.175rem"
+          gradientColorOne={colors.primaryColor}
+          gradientColorTwo={colors.secondaryColor}
+          gradientBorderSize={sizes.borderSize}
           onClick={() => console.log("yup")}
+          unhoveredTextColor={colors.generalWhite}
+          hoveredTextColor={colors.generalWhite}
+          buttonColor={colors.generalBlack}
         >
           This is a long button
         </GradientButton>

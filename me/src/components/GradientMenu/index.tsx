f8ae234ebce8cd2711d267bc54a151c57ed9efb7
Adam Judd <adam.judd@rea-group.com.au>
2021-05-11 22:41:12 +1000

    Add gradient underline on menu item hover

--- me/src/components/GradientMenu/index.tsx
+++ me/src/components/GradientMenu/index.tsx
@@ -2,6 +2,9 @@ import { Menu, MenuItemProps, Segment } from "semantic-ui-react";
 import { GradientMenuProps } from "./props";
 import React, { useState } from "react";
 import styled from "styled-components";
+import { linearGradientStyle } from "../../Helpers/paletteHelper";
+import { colors } from "../../Helpers/palette";
+import { sizes } from "../../Helpers/sizes";
 
 export const GradientMenu = (props: GradientMenuProps) => {
   return (
@@ -14,8 +17,19 @@ export const GradientMenu = (props: GradientMenuProps) => {
 };
 
 export const GradientMenuItem = styled(Menu.Item)`
-  &:hover {
-    background: blue !important;
+  &:hover::after {
+    content: "";
+    position: absolute;
+    border-radius: 100rem;
+    left: 0;
+    bottom: 0;
+    height: ${sizes.borderSize};
+    width: 100%;
+    background: ${linearGradientStyle(
+      45,
+      colors.primaryColor,
+      colors.secondaryColor
+    )};
   }
-  ${(props: MenuItemProps) => props.active ? `` : ``}
+  ${(props: MenuItemProps) => (props.active ? `` : ``)}
 `;

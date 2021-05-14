d30bafca95bf2e5c099658da1ac8e0ab74661c80
Adam Judd <adam.judd@rea-group.com.au>
2021-05-14 12:07:34 +1000

    Fix element position calculations

--- me/src/Helpers/functions.ts
+++ me/src/Helpers/functions.ts
@@ -1,18 +1,13 @@
 export const movingNodeInsideStaticNode = (
-  staticNode: React.RefObject<HTMLDivElement>
-) => (movingNode: React.RefObject<HTMLDivElement>) => {
-  const movingNodeTop = movingNode.current?.offsetTop;
-  const movingNodeHeight = movingNode.current?.offsetHeight;
-  const staticNodeTop = staticNode.current?.offsetTop;
-  const staticNodeHeight = staticNode.current?.offsetHeight;
-  console.log(movingNode, staticNode);
-  if (movingNodeTop && movingNodeHeight && staticNodeTop && staticNodeHeight) {
-    console.log(movingNodeTop);
-    const staticNodeBottom = staticNodeTop + staticNodeHeight;
-    const movingNodeBottom = movingNodeTop + movingNodeHeight;
-    return (
-      staticNodeTop < movingNodeBottom && movingNodeBottom < staticNodeBottom
-    );
-  }
-  return false;
+  movingNode: React.RefObject<HTMLDivElement>
+) => (staticNode: React.RefObject<HTMLDivElement>) => {
+  const movingNodeBottom =
+    movingNode.current?.getBoundingClientRect().bottom ?? 0;
+  const staticNodeTop = staticNode.current?.getBoundingClientRect().top ?? 0;
+  const staticNodeBottom =
+    staticNode.current?.getBoundingClientRect().bottom ?? 0;
+
+  return (
+    staticNodeTop < movingNodeBottom && movingNodeBottom < staticNodeBottom
+  );
 };

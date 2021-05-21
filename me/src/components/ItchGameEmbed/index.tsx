6e89ed972afad92511902de964153e9a90b31fd5
Adam Judd <62adamjudd@gmail.com>
2021-05-21 19:06:23 +1000

    Add game embed image fetching function

--- me/src/components/ItchGameEmbed/index.tsx
+++ me/src/components/ItchGameEmbed/index.tsx
@@ -1,6 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ItchGameEmbedProps } from "./props";
 
-export const ItchGameEmbed: React.FC<ItchGameEmbedProps> = (props) => {
-  return <></>;
+type itchGameData = {
+  title: string;
+  sale: boolean;
+  id: number;
+  cover_image: string;
+  original_price: boolean;
+  price: string;
+};
+
+export const ItchGameEmbed: React.FC<ItchGameEmbedProps> = ({
+  gameName,
+  gamePublisher,
+  gameSlug,
+  description,
+}) => {
+  const [imageUrl, setImageUrl] = useState<string | false>(false);
+  useEffect(() => {
+    const validateGameData = (
+      response: any
+    ): response is itchGameData => {
+      return true;
+    };
+
+    const fetchGameImage = async (publisher: string, slug: string) =>
+      await fetch(`https://${publisher}.itch.io/${slug}/data.json`)
+        .then((data) => data.json())
+        .then((response) =>
+          setImageUrl(validateGameData(response) && response.cover_image)
+        );
+
+    fetchGameImage(gamePublisher, gameSlug);
+  }, [gamePublisher, gameSlug]);
+
+  return (
+    <>
+      <img
+        src={imageUrl ? imageUrl : ""}
+        alt={`${gamePublisher}'s ${gameName} game`}
+      />
+    </>
+  );
 };
